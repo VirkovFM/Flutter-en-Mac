d314f9b8babd9b3cb40d6cb668f7bccfc734c094
VJFM <virkovjarodfloresmedina@gmail.com>
2023-04-24 17:08:14 -0600

    Initial Commit

---
+++ Flutter en Mac/Flutter_en_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flutter_en_MacApp.swift
+//  Flutter en Mac
+//
+//  Created by ISSC_611_2023 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Flutter_en_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
