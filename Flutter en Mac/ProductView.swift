d01a5c515be2e775cd686b71eabc2e241aa84a84
Cesar Elizarraraz Acosta <cea73239@delasalle.edu.mx>
2023-04-26 20:23:07 -0600

    product and Sales done

--- Flutter en Mac/ProductView.swift
+++ Flutter en Mac/ProductView.swift
@@ -11,43 +11,42 @@ struct ProductView: View {
     
     @State private var Id = ""
     @State private var name = ""
-    @State private var lastName = ""
-    @State private var age = ""
-    @State private var gender = ""
-    @State private var email = ""
-    @State private var password = ""
-    @State private var rol = ""
+    @State private var description = ""
+    @State private var units = ""
+    @State private var cost = ""
+    @State private var price = ""
+    @State private var utility = ""
     @State private var showAlert = false
     
     var body: some View {
         Color(#colorLiteral(red: 0.224, green: 0.224, blue: 0.224, alpha: 1)).edgesIgnoringSafeArea(.vertical).overlay(
         VStack{
             
-            Text("PRODUCT").font(.largeTitle).padding()
+            Text("Product").font(.largeTitle).padding().foregroundColor(Color.white).overlay(
+                Text("Product").font(.largeTitle).padding().foregroundColor(Color(red: 0.596, green: 0.694, blue: 0.769)).blur(radius: 5))
             
             Form{
                 
                 TextField("ID", text: $Id)
-                TextField("NAME", text: $name)
-                TextField("LASTNAME", text: $lastName)
-                TextField("AGE", text: $age)
-                TextField("GENDER", text: $gender)
-                TextField("EMAIL", text: $email)
-                TextField("PASSWORD", text: $password)
-                TextField("ROL", text: $rol)
-            }.padding(.horizontal, -17).scrollContentBackground(.hidden)
+                TextField("Name", text: $name)
+                TextField("Last Name", text: $description)
+                TextField("Age", text: $units)
+                TextField("Gender", text: $cost)
+                TextField("Email", text: $price)
+                TextField("Password", text: $utility)
+            }.padding().scrollContentBackground(.hidden)
                 
             
-            Button("HIGH"){
+            Button("High"){
                 //mos
                 //mostrarAlerta = true
                 //print($nombre)
-                if(Id == "" || name == "" || lastName == "" || age == "" || gender == "" || email == "" || password == "" || rol == ""){
+                if(Id == "" || name == "" || description == "" || units == "" || cost == "" || utility == "" || price == "" ){
                     showAlert = true
                     
                 }
                 
-            }.frame(minWidth: 0, maxWidth: 100).padding().buttonStyle(grayButton())
+            }.frame(minWidth: 0, maxWidth: 100).buttonStyle(grayButton())
                 .alert(isPresented: $showAlert){
                     Alert(title: Text("Tonto"), message: Text("Llene los campos"))
                 }
@@ -63,3 +62,5 @@ struct ProductView_Previews: PreviewProvider {
         ProductView()
     }
 }
+
+
--- Flutter en Mac/SalesView.swift
+++ Flutter en Mac/SalesView.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct SalesView: View {
+    
+    @State private var Id = ""
+    @State private var name = ""
+    @State private var quantity = ""
+    @State private var IDV = ""
+    @State private var IDC = ""
+    @State private var pieces = ""
+    @State private var subtotal = ""
+    @State private var total = ""
+    @State private var showAlert = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Color(#colorLiteral(red: 0.224, green: 0.224, blue: 0.224, alpha: 1)).edgesIgnoringSafeArea(.vertical).overlay(
+        VStack{
+            
+            Text("Sales").font(.largeTitle).padding().foregroundColor(Color.white).overlay(
+                Text("Sales").font(.largeTitle).padding().foregroundColor(Color(red: 0.596, green: 0.694, blue: 0.769)).blur(radius: 5))
+            
+            Form{
+                
+                TextField("ID", text: $Id)
+                TextField("Name", text: $name)
+                TextField("Last Name", text: $quantity)
+                TextField("Age", text: $IDV)
+                TextField("Gender", text: $IDC)
+                TextField("Email", text: $pieces)
+                TextField("Password", text: $subtotal)
+                TextField("Rol", text: $total)
+            }.padding().scrollContentBackground(.hidden)
+                
+            
+            Button("High"){
+                //mos
+                //mostrarAlerta = true
+                //print($nombre)
+                if(Id == "" || name == "" || quantity == "" || IDV == "" || IDC == "" || pieces == "" || subtotal == "" || total == ""){
+                    showAlert = true
+                    
+                }
+                
+            }.frame(minWidth: 0, maxWidth: 100).buttonStyle(grayButton())
+                .alert(isPresented: $showAlert){
+                    Alert(title: Text("Tonto"), message: Text("Llene los campos"))
+                }
+                /*.alert(isPresented: $mostrarAlerta){
+                Alert(title: Text("Hola"), message: Text("Has presionado el boton"))
+            }*/
+        })
     }
 }
-
 struct SalesView_Previews: PreviewProvider {
     static var previews: some View {
         SalesView()
