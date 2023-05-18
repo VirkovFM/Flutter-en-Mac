ef9f4af357c77f312259fb3b12cb91005482c2d1
VirkovFM <vfm72673@udelasalle.edu.mx>
2023-05-18 02:22:51 -0700

    FirebaseFuncional

--- Flutter en Mac/View/Flutter_en_MacApp.swift
+++ Flutter en Mac/View/Flutter_en_MacApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Flutter_en_MacApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
